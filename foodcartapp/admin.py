be1b5d1c650d0f85e1067a0a83ff2d09a98dedbe
Denis Polyakov <p-ov.da@ya.ru>
2022-08-10 20:21:24 +0300

    Add redirect to manager menu after edit order

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
+from star_burger import settings
 from .models import Order
 from .models import OrderElements
 from .models import Product
@@ -121,6 +124,17 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderElementsInline]
+
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+        if "next" in request.GET:
+            if url_has_allowed_host_and_scheme(
+                request.GET['next'],
+                allowed_hosts=settings.ALLOWED_HOSTS
+            ):
+                return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
 
 
 @admin.register(OrderElements)
